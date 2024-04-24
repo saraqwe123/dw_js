0068d942ff3fea8b7a88fd7cf14734114d5a67e9
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-04-24 11:48:29 -0300

    24/04

--- contador/javascript.js
+++ contador/javascript.js
@@ -2,21 +2,13 @@ const inputTexto = document.querySelector("[input-texto]")
 const btnContar = document.querySelector("[btn-contar]")
 const divContador = document.querySelector("[txt-contador]")
 
+let tipo = "caracteres"
+inputTexto.addEventListener("input", (evento) => {
 
-let contagemCaracteres = True
-
-const conta_caracteres = (evento) => {
-    console.log(evento.target.value)
-}
-
-
-function conta_caracteres(evento) {
-    if(contagemCaracteres) {
-        evento.target.innerText = 'Contar palavras'
+    if(tipo === "caracteres") {
+        let contar = evento.target.value.lenght;
+        divContador.innerText = contar; 
     } else {
-        evento.target.innerText = 'Contar caracteres'
+
     }
-} 
-
-
-btnContar.addEventListener("click", conta_caracteres) //função resposnsável por capturar evento e tem 2 parametros
+})
