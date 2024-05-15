3d5f52b00e6f68ada66e847834b4bbb462af59e7
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-05-15 09:58:51 -0300

    15/05

--- gerador_link/javascript.js
+++ gerador_link/javascript.js
@@ -1,17 +1,39 @@
-const teste_numero = /^(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})$/; //expressao retirada através da leitura do material disponível para o projeto
+const teste_numero = /^(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})$/; 
+//expressao retirada através da leitura do material disponível para o projeto
+const paragrafo = document.getElementById('parag');
+const link = document.getElementById('span');
 
-function verifica_numero() {
+
+function verifica_numero(event) {
+    console.log(event.target.id)
     const numero = document.getElementById('inumero')
-    if ( teste_numero.test(numero.value)) { 
-        console.log(numero)
-        numero.classList.add('aparecer')
-        console.log(numero)
+    if (teste_numero.test(numero.value)) { 
+        numero.classList.add('certo')
+        let apenas_numero = ""
+        for (let n of numero.value) {
+// a exclamação inverte tudo
+            if (!"()- ".includes(n)) { 
+               apenas_numero += n
+// pegando apenas os numeros para colocar no link
+            }
+        }
+        const gera_link = `https://wa.me/${apenas_numero}` //criando o link 
+        if(event.target.id === 'criar_link') {
+            console.log('sara')
+            link.innerText = gera_link
+            paragrafo.style.display = "block"
+            link.style.display = "block"
+
+        }
+
     }
     //usamos o test para validar expressoes regulares. Neste caso, estamos verificando se o numero é regular
     else {
-        alert('Por favor, digite um número válido!')
+        numero.classList.add('erro')
     }
 }
 
 document.getElementById('criar_link').addEventListener('click', verifica_numero);
 document.getElementById('envia_link').addEventListener('click', verifica_numero);
+
+
