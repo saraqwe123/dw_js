a3f1b48c60e74469df9b46922226e6442e40c199
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-06-07 10:59:58 -0300

    .

--- tarefas/javascript.js
+++ tarefas/javascript.js
@@ -2,6 +2,7 @@ let input = document.getElementById("itarefa")
 let criadas = document.getElementById("tarefas_criadas")
 let feitas = document.getElementById("tarefas_concluidas")
 let formulario = document.getElementById("formulario");
+let lista = document.querySelector("imagem")
 
 let tarefas = []
 
@@ -12,7 +13,20 @@ function conta_tarefas (event) {
         let novaTarefaJSON = { "tarefa": input };
         tarefas.push(input.value)
         criadas.innerText = "Tarefas Criadas" + " " + tarefas.length
+        input.value = ""
     }
 }
 
 formulario.addEventListener("submit", conta_tarefas);
+
+function mostrar_tarefas(event) {
+    event.preventDefault(); 
+
+    if (tarefas.length !== 0) {
+        console.log(tarefas.length)
+        lista.remove()
+    }
+}
+
+
+formulario.addEventListener("submit", mostrar_tarefas);
